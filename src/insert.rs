9766135f7aecc00c2ebcbe0ba86a3f1589868a97
ilyes-guy <random_dude_233@proton.me>
2023-04-23 18:08:35 +0200

    diags

--- src/insert.rs
+++ src/insert.rs
@@ -1,3 +1,11 @@
+struct User {
+    id: u32,
+    username: String,
+    email: String,
+}
+
+// insert into users(id, username, email) values('fze', 'efz', 'fezf');
 pub fn insert (query: String) {
     println!("the insert query is: {}", query);
+    let query_parts: Vec<&str> = query.split(" ").collect();
 }
